8edf092855183dcb5a7cd8dd78cf9ee93509b1e7
khenryjoe <joseph.ketterer@gmail.com>
2019-10-01 12:14:53 +0100

    delete added

--- api/post/read.php
+++ api/post/read.php
@@ -2,8 +2,9 @@
   // Headers
   header('Access-Control-Allow-Origin: *');
   header('Content-Type: application/json');
-  include_once './../config/Database.php';
-  include_once './../models/Post.php';
+  include_once '../../config/Database.php';
+  include_once '../../models/Post.php';
+
   // Instantiate DB & connect
   $database = new Database();
   $db = $database->connect();
@@ -13,6 +14,7 @@
   $result = $post->read();
   // Get row count
   $num = $result->rowCount();
+  
   // Check if any posts
   if($num > 0) {
     // Post array
